04b21858c6e51641542e3d854428ebe8e8310c94
dhipgraby <keneth.zambrano4@gmail.com>
2023-12-21 10:17:50 +0100

    First commit

---
+++ src/data/donateAd.js
@@ -0,0 +1,49 @@
+export const donateData = {
+    key:"donate-ad",
+    title:"Contribute to TxCity!",
+    html:	`<div class="ta-l">
+    <span class="t-yellow">Crypto citizens!</span>
+    <br />
+    Now you can contribute to develop the TxCity, your donations will help to develop features.						
+    <br />
+    <div class="contributeInner">
+    Here is a list of next improvements:
+    <br />
+    <ul>
+    <li>
+    - Add Chains : LTC,XMR,ARBITRUM
+    </li>
+    <li>
+    - Renting houses interface
+    </li>
+    <li>
+    - Show wallet info (Tokens, NFT)
+    </li>				
+    <li>
+    - TxCity Coin (ERC20)
+    </li>						
+    </ul>																		
+    </div>
+
+    <div class="contributeInner">
+    <b>Donation address</b>
+    <ul>
+    <li>
+    <span class="t-yellow">ETH, Polygon or any EVM blockchain:</span
+    <br/>
+    0x4918251174D1E46cc17a848717152A7df47724c8</li>
+    <li>
+    <span class="t-yellow">BTC :</span>
+    <br/> 
+    bc1qz95xpc5c9wwfxmlhhmn4gptqf6h8v5nglem6th
+    </li>						
+    </ul>									
+    </div>								
+    Addresses that donate, will be white listed for future rewards.			
+    <br/>
+    Feel free to give your feed back about what features or services will be good to have.						
+    <br/>
+    <a href="https://n8u0k9wgiua.typeform.com/to/yxcnqZou" target="_blank">Feed back HERE</a>
+    </div>
+    `
+}
